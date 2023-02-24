1bf0f3f596786953e8fc28b662077426a0ee468b
John Costa <johncosta027@gmail.com>
2023-02-24 15:04:14 +0000

    adding more scripts

---
+++ Scripts/OpenProdApps.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+google-chrome-stable --app="https://linear.app/johntech/team/JOH/active" --class=WebApp-Linear7107 --user-data-dir=/home/johnc/.local/share/ice/profiles/Linear7107 &
+
+raindrop &
+
+google-chrome-stable --app="https://calendar.google.com" --class=WebApp-Calendar9598 --user-data-dir=/home/johnc/.local/share/ice/profiles/Calendar9598 &
---
+++ Scripts/ReplaceKdePanel.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm ~/.config/plasma-org.kde.plasma.desktop-appletsrc
+cp ../kde/plasma-org.kde.plasma.desktop-appletsrc ~/.config
