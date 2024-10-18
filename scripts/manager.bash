cad5894fd073a3e3793688578c5aa136d781d058
John Costa <johncosta027@gmail.com>
2024-10-18 10:44:41 +0100

    fix: conditional logic for hyprland minotors

--- scripts/manager.bash
+++ scripts/manager.bash
@@ -4,6 +4,17 @@ git pull
 
 folders=("hypr" "ags" "tmux" "lazygit" "kitty" "fish" "nvim")
 
+# Replace the monitors part of hyprland config
+
+current_host=""
+if [[ $(hostname) == "johnc-desktop" ]]; then
+	current_host="DESKTOP"
+else
+	current_host="LAPTOP"
+fi
+
+sed -i "s/#$current_host: //" ./hypr/hyprland/monitors.conf
+
 for folder in ${folders[@]}; do
   echo Moving $folder
   rsync -a ./$folder/ ~/.config/$folder
@@ -11,3 +22,7 @@ done
 
 # Starship
 cp starship.toml ~/.config/starship.toml
+
+# Cleanup
+
+sed -i "s/^monitor/#$current_host: monitor/" ./hypr/hyprland/monitors.conf
