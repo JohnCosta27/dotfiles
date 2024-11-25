156cba158a3f175c114b5401fdcc0ccdd5446d58
John Costa <johncosta027@gmail.com>
2024-11-25 17:14:02 +0000

    tabs

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,6 +25,13 @@ vim.opt.signcolumn = "yes"
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Tabs
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- Modules --
 
 -- Lazy = Package Manager --
