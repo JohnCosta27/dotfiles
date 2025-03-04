60bfd74d8a558bdd7950b0c27e3489cb288fdfed
John Costa <johncosta027@gmail.com>
2025-03-04 18:00:43 +0000

    feat: big rework. Minimal hyprland setup

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,217 @@
+return {
+	{ "wakatime/vim-wakatime", lazy = false },
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					rust = { "rustfmt", lsp_format = "fallback" },
+					javascript = { "prettierd", "prettier", stop_after_first = true },
+				},
+				format_on_save = {
+					lsp_format = "fallback",
+				},
+			})
+		end,
+	},
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			--[[
+			require("lint").linters_by_ft = {
+				javascript = {
+					"eslint_d",
+				},
+				typescript = {
+					"eslint_d",
+				},
+				javascriptreact = {
+					"eslint_d",
+				},
+				typescriptreact = {
+					"eslint_d",
+				},
+			}
+            ]]
+			--
+		end,
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
+		},
+	},
+	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{ "Everblush/nvim", name = "everblush" },
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.gruvbox_material_enable_italic = true
+			vim.g.gruvbox_material_background = "hard"
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
+	{ "hrsh7th/vim-vsnip" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "vsnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			-- require("telescope").load_extension("fzf")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = { "lua", "javascript", "typescript", "html", "tsx", "zig" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+			{
+				"s1n7ax/nvim-window-picker",
+				version = "2.*",
+				config = function()
+					require("window-picker").setup({
+						filter_rules = {
+							include_current_win = false,
+							autoselect_one = true,
+							-- filter using buffer options
+							bo = {
+								-- if the file type is one of following, the window will be ignored
+								filetype = { "neo-tree", "neo-tree-popup", "notify" },
+								-- if the buffer type is one of following, the window will be ignored
+								buftype = { "terminal", "quickfix" },
+							},
+						},
+					})
+				end,
+			},
+		},
+		config = function()
+			require("neo-tree").setup({
+				filesystem = {
+					follow_current_file = {
+						enabled = true,
+					},
+				},
+				close_if_last_window = false,
+			})
+			vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp" },
+		config = function()
+			local lsp = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			lsp.lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim", "require" } },
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+					},
+				},
+			})
+
+			lsp.vtsls.setup({
+				capabilities = capabilities,
+			})
+
+			lsp.zls.setup({
+				capabilities = capabilities,
+			})
+
+			lsp.rust_analyzer.setup({
+				capabilities = capabilities,
+				diagnostic = {
+					-- Prevents the annoying popup for cancalled requests
+					refreshSupport = false,
+				},
+			})
+
+			lsp.gopls.setup({
+				capabilities = capabilities,
+			})
+			lsp.tailwindcss.setup({
+				capabilities = capabilities,
+			})
+			lsp.ocamllsp.setup({
+				capabilities = capabilities,
+			})
+			lsp.svelte.setup({
+				capabilities = capabilities,
+			})
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+	},
+}
--- scripts/manager.bash
+++ scripts/manager.bash
@@ -2,27 +2,19 @@
 cd ~/Code/dotfiles
 git pull
 
-folders=("hypr" "ags" "tmux" "lazygit" "kitty" "fish" "nvim")
-
 # Replace the monitors part of hyprland config
 
-current_host=""
-if [[ $(hostname) == "johnc-desktop" ]]; then
-	current_host="DESKTOP"
-else
-	current_host="LAPTOP"
-fi
+# current_host=""
+# if [[ $(hostname) == "johnc-desktop" ]]; then
+# 	current_host="DESKTOP"
+# else
+# 	current_host="LAPTOP"
+# fi
+#
+# sed -i "s/#$current_host: //" ./hypr/hyprland/monitors.conf
 
-sed -i "s/#$current_host: //" ./hypr/hyprland/monitors.conf
-
-for folder in ${folders[@]}; do
-  echo Moving $folder
-  rsync -a ./$folder/ ~/.config/$folder
-done
-
-# Starship
-cp starship.toml ~/.config/starship.toml
+rsync -av .config ~ --delete
 
 # Cleanup
 
-sed -i "s/^monitor/#$current_host: monitor/" ./hypr/hyprland/monitors.conf
+# sed -i "s/^monitor/#$current_host: monitor/" ./hypr/hyprland/monitors.conf
