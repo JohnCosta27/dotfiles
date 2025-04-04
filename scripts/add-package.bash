6e2137daefee9f30620bc111df70cd739a0d2c2b
John Costa <johncosta027@gmail.com>
2025-04-04 10:58:22 +0100

    feat: adding tmux and renaming config folder

--- scripts/add-package.bash
+++ scripts/add-package.bash
@@ -6,8 +6,8 @@ if [ -z  $@ ]; then
     exit 1
 fi
 
-echo "metapac add --backend arch --package $@"
-metapac add --backend arch --package $@
+echo "metapac add --backend arch --packages $@"
+metapac add --backend arch --packages $@
 
 rsync -av ~/.config/metapac .config --delete
 
