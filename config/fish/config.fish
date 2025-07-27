a60263aa7868e18c9b7c38aade68d6199170039f
John Costa <johncosta027@gmail.com>
2025-07-27 21:42:37 +0100

    feat: adding shortcuts to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,5 +5,7 @@ set -gx ANDROID_HOME $HOME/Android/Sdk
 set -gx NDK_HOME $ANDROID_HOME/ndk/$(ls -1 $ANDROID_HOME/ndk)
 
 alias add-pkg="bash ~/Code/dotfiles/scripts/add-package.bash"
+alias lg="lazygit"
+alias v="nvim"
 
 starship init fish | source
