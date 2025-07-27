aaa5b19a8608ddd144ef0611bf4f2b0b5f02bd9d
John Costa <johncosta027@gmail.com>
2025-07-27 21:34:12 +0100

    feat: removing old stuff

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,11 +2,11 @@
 vim.g.mapleader = " "
 
 -- Guard (Linters + Formatters)
-vim.g.guard_config = {
-	fmt_on_save = true,
-	lsp_as_default_formatter = true,
-	save_on_fmt = true,
-}
+-- vim.g.guard_config = {
+-- 	fmt_on_save = true,
+-- 	lsp_as_default_formatter = true,
+-- 	save_on_fmt = true,
+-- }
 
 -- Sync system clipboard and neovim register
 vim.opt.clipboard = { "unnamedplus" }
